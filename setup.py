71b2b4c91a93b86a01654dfb9a67d5304c3385d2
Friedrich Lindenberg <friedrich@pudo.org>
2021-07-07 14:24:46 +0200

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as f:
 
 setup(
     name="prefixdate",
-    version="0.3.2",
+    version="0.3.3",
     description="Formatting utility for international postal addresses",
     long_description=long_description,
     long_description_content_type="text/markdown",
